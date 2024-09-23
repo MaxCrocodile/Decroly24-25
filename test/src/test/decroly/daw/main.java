eaf861da9180545706ca5d68b4a9753790560d91
COMPU-S032\daw1 <bodean0375@gmail.com>
2024-09-23 11:31:58 +0200

    primer ejemplo

---
+++ test/src/test/decroly/daw/main.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package test.decroly.daw;
+
+/**
+ * 
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.print("Hello World");
+		//System.out.println("Hello Decroly DAW");
+		
+		int num = 0;;
+		int a = 23;
+		//String text1 = "commo te llama? \n";
+		
+		String text2 = "Paco";
+		
+		System.out.println(text1 + text2);
+
+		boolean isEnable = false;
+		
+		System.out.println((a == num));
+		
+
+	}
+
+}
