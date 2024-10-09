cc39d0b5a1bd5eeb64987592b2cfb44d55ccf742
COMPU-S032\daw1 <bodean0375@gmail.com>
2024-10-09 10:07:02 +0200

    act4

--- Actividad4/src/Ejercicio3/Ejercicio3.java
+++ Actividad4/src/Ejercicio3/Ejercicio3.java
@@ -10,12 +10,24 @@ public class Ejercicio3 {
 		
 		System.out.println("Introduce 10 numeros:");
 		for (int i = 0 ; i < 10; i++) {
-			System.out.println("Numero" + i+1 + ":");
+			System.out.println("Numero " + i+1 + ":");
 			numeros[i] = sc.nextDouble();
 		}
-		
-		
-		
+		double max = numeros[0];
+        double min = numeros[0];
+        
+       
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] > max) {
+                max = numeros[i];
+            }
+            if (numeros[i] < min) {
+                min = numeros[i];
+            	}
+        	}
+        
+        System.out.println("El número máximo es: " + max);
+        System.out.println("El número mínimo es: " + min);
 		
 		sc.close();
 	}
