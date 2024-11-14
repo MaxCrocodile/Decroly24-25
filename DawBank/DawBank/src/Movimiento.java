c39f5e68c4a090d9565b12e2e8b083105498f565
DESKTOP-P8FMSPB\Max <bodean0375@gmail.com>
2024-11-14 12:37:23 +0100

    DAWBank v.final CHATGBT

--- DawBank/DawBank/src/Movimiento.java
+++ DawBank/DawBank/src/Movimiento.java
@@ -1,6 +1,25 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Movimiento {
-    private int ID;
-    private int fecha;
-    private String TIPO;
-    private int cantidad;
+    private static int contador = 1;
+
+    private int id;
+    private String fecha;
+    private String tipo;
+    private double cantidad;
+
+    public Movimiento(String tipo, double cantidad) {
+        this.id = contador++;
+        this.fecha = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+    }
+
+    public void mostrarInfoMovimiento() {
+        System.out.println("ID: " + id);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Cantidad: " + cantidad);
+    }
 }
